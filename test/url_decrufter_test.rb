e99c36026213b23fe2595c3e23bea9121a90fe00
Michael Josephson <mike@josephson.org>
2016-01-24 16:56:42 +0000

    rename test

--- test/url_decrufter_test.rb
+++ test/url_decrufter_test.rb
@@ -1,6 +1,6 @@
 require 'minitest_helper'
 
-class TestUrlDecrufter < Minitest::Test
+class UrlDecrufterTest < Minitest::Test
   def test_that_it_has_a_version_number
     refute_nil ::UrlDecrufter::VERSION
   end
