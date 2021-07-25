e5bc78a5d83793203f86ee267ce9a89c83778241
Michael Josephson <30024+mikej@users.noreply.github.com>
2021-07-25 16:10:42 +0100

    initial version that removes utm_ query params

--- lib/url_decrufter.rb
+++ lib/url_decrufter.rb
@@ -2,6 +2,23 @@ require "url_decrufter/version"
 
 module UrlDecrufter
   def self.decruft(url)
-    url
+    uri = URI(url)
+
+    if uri.query.present?
+      params = uri.query.split("&").map { |p| p.split("=") }
+      filtered_params = params.reject do |name, value|
+        name.starts_with? "utm_"
+      end
+
+      # may end up with 0 query params after filtering, in which case set query to
+      # nil to avoid trailing '?' on the URL
+      if filtered_params.size > 0
+        uri.query = filtered_params.map { |name, value | "#{name}=#{value}" }.join("&")
+      else
+        uri.query = nil
+      end
+    end
+
+    url.is_a?(String) ? uri.to_s : uri
   end
 end
