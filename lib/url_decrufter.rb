42359798539cea68442e65369ccb73ebb395aa8a
Michael Josephson <30024+mikej@users.noreply.github.com>
2021-07-30 16:29:15 +0100

    refactor to support multiple filters
    preparation for doing other filtering steps in addition to removal of
    Google Analytics query params.

--- lib/url_decrufter.rb
+++ lib/url_decrufter.rb
@@ -1,24 +1,49 @@
 require "url_decrufter/version"
 
+require "uri"
+
 module UrlDecrufter
+  class UrlFilter
+    attr_accessor :uri
+
+    def initialize(uri)
+      @uri = uri.dup
+    end
+
+    def params
+      return [] if @uri.query.nil?
+      @uri.query.split("&").map { |p| p.split("=") }
+    end
+
+    def update_params(new_params)
+      # may end up with 0 query params after filtering, in which case set query to
+      # nil to avoid trailing '?' on the URL
+      if new_params.size > 0
+        @uri.query = new_params.map { |name, value | "#{name}=#{value}" }.join("&")
+      else
+        @uri.query = nil
+      end
+    end
+  end
+
+  class GoogleAnalytics < UrlFilter
+    def filter
+      filtered_params = params.reject do |name, value|
+        name.start_with? "utm_"
+      end
+      update_params(filtered_params)
+
+      uri
+    end
+  end
+
+  FILTERS = [GoogleAnalytics]
+
   def self.decruft(url)
     uri = URI(url)
 
-    if uri.query.present?
-      params = uri.query.split("&").map { |p| p.split("=") }
-      filtered_params = params.reject do |name, value|
-        name.starts_with? "utm_"
-      end
+    result = FILTERS.reduce(uri) { |uri, filter_class| filter_class.new(uri).filter }
 
-      # may end up with 0 query params after filtering, in which case set query to
-      # nil to avoid trailing '?' on the URL
-      if filtered_params.size > 0
-        uri.query = filtered_params.map { |name, value | "#{name}=#{value}" }.join("&")
-      else
-        uri.query = nil
-      end
-    end
-
-    url.is_a?(String) ? uri.to_s : uri
+    url.is_a?(String) ? result.to_s : result
   end
 end
