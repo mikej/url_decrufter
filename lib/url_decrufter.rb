3e06e0da263ce07b6babb93d40cd12ac8c3c81b4
Michael Josephson <mikej@users.noreply.github.com>
2024-03-12 17:38:20 +0000

    restructure to reduce code duplication in filters
    individual filters now just need to override and implement specific
    logic in `filtered_params`
    
    `filter` method that updates `@uri` with the filtered parameters, and
    returns the filtered URI is now in the base class.
    
    also introduce `filter_applies?` hook so individual filters can override
    this to determine if the particular filter should apply to the URI (e.g.
    could do a domain name check).

--- lib/url_decrufter.rb
+++ lib/url_decrufter.rb
@@ -14,6 +14,10 @@ module UrlDecrufter
       return [] if @uri.query.nil?
       @uri.query.split("&").map { |p| p.split("=") }
     end
+    
+    def filtered_params
+      params
+    end 
 
     def update_params(new_params)
       # may end up with 0 query params after filtering, in which case set query to
@@ -24,41 +28,48 @@ module UrlDecrufter
         @uri.query = nil
       end
     end
+    
+    def filter
+      update_params(filtered_params) if filter_applies?
+      
+      uri
+    end
+    
+    def filtered_params
+      params
+    end
+    
+    def filter_applies?
+      true
+    end
   end
 
   class GoogleAnalytics < UrlFilter
-    def filter
-      filtered_params = params.reject do |name, value|
+    def filtered_params
+      params.reject do |name, value|
         name.start_with? "utm_"
       end
-      update_params(filtered_params)
-
-      uri
     end
   end
 
   class GUCE < UrlFilter
-    def filter
-      filtered_params = params.reject do |name, value|
+    def filtered_params
+      params.reject do |name, value|
         name.start_with?("guce_") || name == "guccounter"
       end
-      update_params(filtered_params)
-
-      uri
     end
   end
 
   # Filter to strip the `cmdf` query param used by The Magic Highlighter (Safari extension)
   class MagicHighlighter < UrlFilter
-    def filter
-      filtered_params = params.reject do |name, value|
+    def filtered_params
+      params.reject do |name, value|
         name == "cmdf"
       end
-      update_params(filtered_params)
-
-      uri
     end
   end
+  
+  
 
   FILTERS = [GoogleAnalytics, GUCE, MagicHighlighter]
 
